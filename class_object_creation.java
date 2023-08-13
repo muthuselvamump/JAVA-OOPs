911403dbf91297ae3a23497ca04e9227729037a0
muthuselvamump <muthuselvamump@gmail.com>
2023-08-13 21:57:15 +0530

    class and object

--- class_object_creation.java
+++ class_object_creation.java
@@ -7,10 +7,11 @@ class box{ //class
 public class class_object_creation{
     public static void main(String [] args){
         box blackbox; //creating object or instance
-        blackbox=new box();
+        blackbox=new box(); //instance memeory intialized
 
         blackbox.length=20;
         System.out.println(blackbox.length);
+        System.out.println(blackbox.breadth);
 
     }
 }
