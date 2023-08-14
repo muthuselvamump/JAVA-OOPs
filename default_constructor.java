b053cfc7045ae1bf478a56c6b365d0d29a6b577e
muthuselvamump <muthuselvamump@gmail.com>
2023-08-14 23:30:52 +0530

    default_constructor

---
+++ default_constructor.java
@@ -0,0 +1,11 @@
+class box1{
+    box1(){
+        System.out.println("box instance created");
+    }
+}
+public class default_constructor {
+    public static void main(String []args){
+        box1 box=new box1();
+    }
+    
+}
