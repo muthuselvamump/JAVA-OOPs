9770f94c82c187d47deb884777f4cc46783c16e1
muthuselvamump <muthuselvamump@gmail.com>
2023-08-15 18:53:29 +0530

    without_argument_method

---
+++ without_argument_method.java
@@ -0,0 +1,9 @@
+public class without_argument_method {
+    static void dotprint(){
+        System.out.println("..............................................");
+    }
+    public static void main(String []args){
+        dotprint();
+    }
+    
+}
