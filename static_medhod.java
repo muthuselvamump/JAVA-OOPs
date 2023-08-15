1330fadff5e3f3a3debcb6c7ad3ce5ff258c6b4e
muthuselvamump <muthuselvamump@gmail.com>
2023-08-15 16:15:53 +0530

    static_method

---
+++ static_medhod.java
@@ -0,0 +1,12 @@
+public class static_medhod {
+    static int boxvolume(int length, int breadth,int height){ //we didnt create any object so its an static method
+        int vol;
+        vol=length*breadth*height;
+        return vol;
+    }
+    public static void main(String[] args){
+        int volume=boxvolume(10,15,3); //assing parametre or argument
+        System.out.println(volume);
+    }
+    
+}
