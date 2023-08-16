da5edf86376df9fa6b7deae2821b1e336514f43b
muthuselvamump <muthuselvamump@gmail.com>
2023-08-16 23:07:24 +0530

    invoke_method

---
+++ invoke_method.java
@@ -0,0 +1,15 @@
+class box{
+    int length;
+    int breadth;
+    int height;
+int volume(){
+    return length*breadth*height;
+}
+
+}
+public class invoke_method {
+    public static void main(String []args){
+
+    }
+    
+}
