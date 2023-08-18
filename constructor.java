c982af47beadf71b0721bcea58ddb06b35a517f3
muthuselvamump <muthuselvamump@gmail.com>
2023-08-18 23:58:12 +0530

    constructor

---
+++ constructor.java
@@ -0,0 +1,14 @@
+class box{
+    int a;
+    int b;
+    int c;
+    box(int d,){
+
+    }
+}
+public class constructor {
+    public static void main(String []args){
+        System.out.println();
+    }
+    
+}
