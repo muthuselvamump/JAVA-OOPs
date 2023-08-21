e68cab048cc360e27fd794292378e3158ebae732
muthuselvamump <muthuselvamump@gmail.com>
2023-08-21 22:45:49 +0530

    garbage_collection

---
+++ garbage_collection.java
@@ -0,0 +1,31 @@
+class test{
+    int testcase1;
+    int testcase2;
+    int testcase3;
+    test(int a,int b,int c){
+        testcase1=a;
+        testcase2=b;
+        testcase3=c;
+
+    }
+    void testcasedetails(){
+        System.out.println("testcase1 result is: "+ testcase1 + "%");
+        System.out.println("testcase2 result is: "+ testcase2 + "%");
+        System.out.println("testcase3 result is: "+ testcase3 + "%");
+    }
+}
+public class garbage_collection {
+    public static void main(String []args){
+        test newtest1=new test(80, 40, 64);
+        test newtest2=new test(88, 91, 78);
+
+        newtest1.testcasedetails();
+        newtest2.testcasedetails();
+        newtest1=null;
+        newtest1=newtest2;
+        newtest1.testcasedetails();
+
+
+    }
+    
+}
