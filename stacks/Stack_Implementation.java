74d25469792f7c0b75908582c48e4f55c54f2c47
muthuselvamump <muthuselvamump@gmail.com>
2023-08-22 23:11:16 +0530

    stack_implementation

---
+++ stacks/Stack_Implementation.java
@@ -0,0 +1,7 @@
+package stacks;
+public class Stack_Implementation{
+    public static void main(String []args){
+
+    }
+
+}
---
+++ stacks/stack.java
@@ -0,0 +1,6 @@
+package stacks;
+
+public class stack {
+    int arr[]=new int[10];
+    
+}
