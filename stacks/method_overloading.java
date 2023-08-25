9d628d784866f8b2cb8f0df77c46acdc74d9e7fa
muthuselvamump <muthuselvamump@gmail.com>
2023-08-25 23:49:33 +0530

    stack_implementation

--- stacks/method_overloading.java
+++ stacks/method_overloading.java
@@ -9,8 +9,14 @@ public class method_overloading {
     static long max(long a,long b){
         return a>b?a:b;
     }
+    static byte max(byte a,byte b){
+        
+        return a>b?a:b;
+        
+    }
     public static void main(String []args){
         System.out.println(max(5,6));
+        System.out.println(max(-1,-2));
         System.out.println(max(5.5,6.8));
         System.out.println(max(65424524,85542425));
         
