d271d5f47ee50c3f1a93e132b8fb9734b729ddaf
muthuselvamump <muthuselvamump@gmail.com>
2023-08-29 22:55:58 +0530

    method_overloading_2

---
+++ method_overloading_2.java
@@ -0,0 +1,9 @@
+public class method_overloading_2 {
+    static void test(){
+        System.out.println("Method without Arguments");
+    }
+    public static void main(String[] args){
+
+    }
+    
+}
