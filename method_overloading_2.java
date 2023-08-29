c78c453f2989c058d8e9c5400567a4ef97f9560f
muthuselvamump <muthuselvamump@gmail.com>
2023-08-29 22:57:53 +0530

    method_overloading_2

--- method_overloading_2.java
+++ method_overloading_2.java
@@ -2,7 +2,16 @@ public class method_overloading_2 {
     static void test(){
         System.out.println("Method without Arguments");
     }
+    static void test(int a){
+        System.out.println("Method with one Arguments");
+    }
+    static void test(int a,int b){
+        System.out.println("Method with two Arguments");
+    }
     public static void main(String[] args){
+        test();
+        test(5);
+        test(5,6);
 
     }
     
