71951d05eceaee50c8769ce001c6e4c36adaeca5
muthuselvamump <muthuselvamump@gmail.com>
2023-09-03 21:49:40 +0530

    copy_constructor

---
+++ copy_constructor.java
@@ -0,0 +1,20 @@
+
+class box{
+    int length;
+    int breadth;
+    int height;
+    box(int a, int b, int c){
+        length=a;
+        breadth=b;
+        height=c;
+
+    }
+
+}
+public class copy_constructor {
+    public static void main(String []args){
+        box
+
+    }
+    
+}
