c89687dd54d04d78ca565c17b2ca9314b8116901
muthuselvamump <muthuselvamump@gmail.com>
2023-09-04 21:09:10 +0530

    queue_implementation

---
+++ queue/queue.java
@@ -0,0 +1,12 @@
+package queue;
+
+public class queue {
+    int arr[]=new int[10];
+    int index;
+    int index2;
+    queue(){
+        index=-1;
+    }
+    en
+    
+}
---
+++ queue/queue_implementation.java
@@ -0,0 +1,10 @@
+package queue;
+
+public class queue_implementation {
+    queue q=new queue();
+    q.enqueue(5);
+    q.enqueue(15);
+    q.enqueue(20);
+    q.dequeue();
+    
+}
