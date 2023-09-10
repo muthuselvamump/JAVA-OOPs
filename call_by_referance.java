ab5db0c9e766dda79911eaa11d6f9f75c89cb34e
muthuselvamump <muthuselvamump@gmail.com>
2023-09-10 23:41:47 +0530

    call_by_referance

--- call_by_referance.java
+++ call_by_referance.java
@@ -7,12 +7,19 @@ class demo{
         height=h;
         breadth=b;
     }
+    void changeval1(demo d){
+        d.length++;
+    }
 
 }
 public class call_by_referance {
     public static void main(String []args){
 
         demo obj=new demo(5,6,8);
+        demo obj1=new demo(25,25,25);   
+        System.out.println(obj1.length);
+        obj.changeval1(obj1);
+        System.out.println(obj1.length);
     }
     
     
