1c14b311a2cbc530acfddeee472e32fa40eb8d04
muthuselvamump <muthuselvamump@gmail.com>
2023-09-11 22:50:49 +0530

    returning_object

---
+++ returning_object.java
@@ -0,0 +1,23 @@
+class Returnobj{
+    int data1;
+    int data2;
+    int data3;
+    Returnobj(int d1,int d2,int d3){
+        data1=d1;
+        data2=d2;
+        data3=d3;
+    }
+    Returnobj doubleval(){
+        Returnobj temp=new Returnobj(data3, data2, data1);
+        return temp;
+    }
+
+}
+public class returning_object {
+    public static void main(String []args){
+        Returnobj obj1=new Returnobj(7,5,6);
+        Returnobj obj2=obj1.doubleval();
+
+    }
+    
+}
