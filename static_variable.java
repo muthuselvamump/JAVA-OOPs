926d1945f3fe848e4558166f2e7a8f917a8dc3f7
muthuselvamump <muthuselvamump@gmail.com>
2023-09-15 22:23:02 +0530

    static_variable

--- static_variable.java
+++ static_variable.java
@@ -1,24 +1,38 @@
-class test{
+class testdemo{
     int data1;
     int data2;
     int data3;
     static int count;
-    test(int a,int b,int c){
+    static{
+        count=0;
+        System.out.println("count intilaized from static block");
+    }
+    testdemo(int a,int b,int c){
         data1=a;
         data2=b;
         data3=c;
         count++;
     }
-    test(){
+    testdemo(){
         count++;
 
     }
-    test(int a){
+    testdemo(int a){
         data1=data2=data3=a;
         count++;
     }
 }
 
 public class static_variable {
+    public static void main(String []args){
+        System.out.println("main starts ");
+        testdemo box;
+        System.out.println("before creating object");
+        box=new testdemo();
+        System.out.println("before creating object");
+        testdemo box1=new testdemo(10);
+        testdemo box2=new testdemo(20,5,4);
+        System.out.println(testdemo.count);
+    }
     
 }
