2a8479de854bfbf3e4d32f233694499f36d203f3
muthuselvamump <muthuselvamump@gmail.com>
2023-09-16 21:43:48 +0530

    Variable_Length_Arguments

--- Variable_Length_Arguments.java
+++ Variable_Length_Arguments.java
@@ -13,8 +13,23 @@ public class Variable_Length_Arguments{
             System.out.println("Minimum Value is : " + min);
         }
     }
+    static void minvalue(double... val){
+        double min=Double.MAX_VALUE;
+        for(double k:val){
+            if(k<min){
+                min=k;
+            }
+        }
+        if(min == Double.MAX_VALUE){
+            System.out.println("invalid arguments");
+        }
+        else{
+            System.out.println("Minimum Value is : " + min);
+        }
+    }
     public static void main(String []args){
         minvalue(5,2,8,-1,6,6);
+        minvalue(4.56,5.67,8.2,5.2,6.5);
         
     }
 }
