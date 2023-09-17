a6c44b41bcb900fc8696fd71560bf91915bbd8fd
muthuselvamump <muthuselvamump@gmail.com>
2023-09-17 12:05:38 +0530

    Command_Line_Arguments

--- Command_Line_Arguments.java
+++ Command_Line_Arguments.java
@@ -1,7 +1,7 @@
 public class Command_Line_Arguments {
     public static void main(String []args){
         for(String k:args){
-            S
+            System.out.println(k);
 
         }
     }
