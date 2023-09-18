ddd4e735bf14eea2a52f44fd91c8aea2992777c8
muthuselvamump <muthuselvamump@gmail.com>
2023-09-18 21:18:35 +0530

    Variable_Length_Arguments

--- Variable_Length_Arguments.java
+++ Variable_Length_Arguments.java
@@ -26,13 +26,20 @@ public class Variable_Length_Arguments{
         else{
             System.out.println("Minimum Value is : " + min);
         }
-    static void test(String a,char b,int... c){
-
     }
+    static void test(String a,char b,int... c){
+        System.out.println(a);
+        System.out.println(b);
+        for(int k:c){
+            System.out.println(k);
+        }
+        
+
+
     }
     public static void main(String []args){
         minvalue(5,2,8,-1,6,6);
         minvalue(4.56,5.67,8.2,5.2,6.5);
-        
+        test("hello",'w',1,2,3,4,5);
     }
 }
