06daf79109755a8c713629e1f777f4e1c2b2a0e3
muthuselvamump <muthuselvamump@gmail.com>
2023-09-19 23:11:30 +0530

    static_variable

--- static_medhod.java
+++ static_medhod.java
@@ -1,10 +1,11 @@
 public class static_medhod {
     static void printstate(){
-        
+        System.out.println("print static medhod");
     }
     static int boxvolume(int length, int breadth,int height){ //we didnt create any object so its an static method
         int vol;
         vol=length*breadth*height;
+        printstate();
         return vol;
     }
     public static void main(String[] args){
