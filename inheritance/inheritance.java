e0bf75c9a031ddf8c4d91989a3ce054785c1d669
muthuselvamump <muthuselvamump@gmail.com>
2023-09-23 20:20:20 +0530

    inheritance

--- inheritance/inheritance.java
+++ inheritance/inheritance.java
@@ -6,12 +6,20 @@ class Employee{
         name=n;
         salary=s;
     }
+    void setName(String name) {
+        this.name= name;
+    }
+    void setSalary(double salary) {
+        this.salary = salary;
+    }
+
         
 }
 
 public class inheritance {
     public static void main(String []args){
         Employee e1=new Employee("RAju", 30000);
+        e1.setSalary(50000);
         System.out.println(e1.salary);
     }
     
