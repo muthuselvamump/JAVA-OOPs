2d7a9f4dd340a3ff09aaef4045a50c857a56ae1c
muthuselvamump <muthuselvamump@gmail.com>
2023-10-03 20:54:03 +0530

    remove_vowels_in_string

---
+++ remove_vowels_in_string.java
@@ -0,0 +1,10 @@
+public class remove_vowels_in_string {
+    public static void main(String []args){
+        String s="welcome";
+        String s1 = "";
+        s1=s.replaceAll("[aeiou]", "");
+        System.out.println(s1);
+
+    }
+    
+}
