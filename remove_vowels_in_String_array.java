9203152e9f8be9d74ca54bfb924623bf2b9dbd8c
muthuselvamump <muthuselvamump@gmail.com>
2023-10-06 21:36:21 +0530

    remove_vowels_in_String_array

--- remove_vowels_in_String_array.java
+++ remove_vowels_in_String_array.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class remove_vowels_in_String_array {
     public static void main(String []args){
-        System.out.println("Enter String");
+        System.out.println("Enter Array Length");
         Scanner scan=new Scanner(System.in);
+        int length;
         int len=3;
         String []str=new String[3];
         for(int i=0;i<len;i++){
@@ -10,7 +11,7 @@ public class remove_vowels_in_String_array {
         }
 
        // String []str={"welcome","helloworld","javadeveloper"};
-        String []str1={"","",""};
+        String []str1=new String[3];
         for(int i=0;i<str.length;i++){
             for(int j=0;j<str[i].length();j++){
                 if(str[i].charAt(j)=='a' || str[i].charAt(j)=='e' || str[i].charAt(j)=='i' || str[i].charAt(j)=='o' || str[i].charAt(j)=='u'){
