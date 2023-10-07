77a3306c113ca236ffd2c0f144d497b03e38cf28
muthuselvamump <muthuselvamump@gmail.com>
2023-10-07 22:37:56 +0530

    remove_vowels_in_String_array

--- remove_vowels_in_String_array.java
+++ remove_vowels_in_String_array.java
@@ -12,12 +12,15 @@ public class remove_vowels_in_String_array {
 
        // String []str={"welcome","helloworld","javadeveloper"};
         String []str1=new String[len];
+         for(int i=0;i<str.length;i++){
+            
+         }
         for(int i=0;i<str.length;i++){
             for(int j=0;j<str[i].length();j++){
                 if(str[i].charAt(j)=='a' || str[i].charAt(j)=='e' || str[i].charAt(j)=='i' || str[i].charAt(j)=='o' || str[i].charAt(j)=='u'){
                     continue;
                 }
-                str1[i] +=str[i].charAt(j);
+                str1[i]=str[i].charAt(j);
                 //System.out.println(str[i].charAt(j));
             }
         }
