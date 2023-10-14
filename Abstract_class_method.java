10f22dfc2dbb554818c3158d963667fb598214bb
muthuselvamump <muthuselvamump@gmail.com>
2023-10-14 22:57:14 +0530

    Abstract_class_method

---
+++ Abstract_class_method.java
@@ -0,0 +1,16 @@
+ abstract class member{
+        abstract void printstatement();
+
+    }
+    class student{
+
+    }
+    class teacher{
+        
+    }
+public class Abstract_class_method {
+    public static void main(String []args){
+
+    }
+    
+}
