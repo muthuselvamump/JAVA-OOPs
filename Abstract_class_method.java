14ced4c96e476cf51357f88fbe90e0c688805f1c
muthuselvamump <muthuselvamump@gmail.com>
2023-10-14 23:04:46 +0530

    Abstract_class_method

--- Abstract_class_method.java
+++ Abstract_class_method.java
@@ -2,15 +2,22 @@
         abstract void printstatement();
 
     }
-    class student{
-
+    class student1 extends member{
+        void printstatement(){
+            System.out.println("student class");
+        }
     }
-    class teacher{
-        
+    class teacher extends member{
+        void printstatement(){
+            System.out.println("teacher class");
+        }
     }
 public class Abstract_class_method {
     public static void main(String []args){
-
+        student1 s=new student1();
+        teacher t=new teacher();
+        s.printstatement();
+        t.printstatement();
     }
     
 }
