590d1f6a79c7c68a0d60e01f5d9d1c7472f67c13
muthuselvamump <muthuselvamump@gmail.com>
2023-10-15 11:53:00 +0530

    Abstract_class_method

--- Abstract_class_method.java
+++ Abstract_class_method.java
@@ -16,8 +16,13 @@ public class Abstract_class_method {
     public static void main(String []args){
         student1 s=new student1();
         teacher t=new teacher();
+       // member m1=new member(); we cant intiate object in abstract class
         s.printstatement();
         t.printstatement();
+        String s1="hello"+6+6+6;
+        System.out.println(s1);
+        member [] m=new member[3];
+        
     }
     
 }
