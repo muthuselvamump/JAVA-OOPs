5363863d7a392311ccfafab167316993937fa3ed
muthuselvamump <muthuselvamump@gmail.com>
2023-10-17 22:20:23 +0530

    Final_Keyword

--- Final_Keyword.java
+++ Final_Keyword.java
@@ -1,11 +1,25 @@
-final class finalclass{
-    final double PI=3.14;
+final class finalclass{  //we cant inherit final class
+}
+class test2{
+    final void testingk(){
+
+    }
 
 }
 
-public class Final_Keyword {
-    public static void main(String []args){
+public class Final_Keyword extends test2{
+    /*void testingk(){  // we cant override final method in derived class
 
+    }*/
+    void testingk(int k){  // but we can overload final method
+        System.out.println("testing");
+
+    }
+    public static void main(String []args){
+        Final_Keyword f=new Final_Keyword();
+        f.testingk(5);
+        final double PI=3.14;
+        //PI=2.5; // we cant change final variable
 
         
     }
