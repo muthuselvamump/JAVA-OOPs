66e625004e955970cd769ec99cf153014f06d852
muthuselvamump <muthuselvamump@gmail.com>
2023-10-30 12:30:36 +0530

    reflection

---
+++ reflection.java
@@ -0,0 +1,17 @@
+class A2{
+    public void method1(){
+        System.out.println("method1");
+    }
+    public void method2(){
+        System.out.println("method2");
+    }
+    public void method3(){
+        System.out.println("method3");
+    }
+}
+public class reflection {
+    public static void main(String []args){
+
+    }
+    
+}
