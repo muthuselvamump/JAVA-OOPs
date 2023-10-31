b978ca9dabf5cadc045f528e6e6655e058a3bfa1
muthuselvamump <muthuselvamump@gmail.com>
2023-10-31 22:58:18 +0530

    reflection

--- reflection.java
+++ reflection.java
@@ -11,11 +11,15 @@ class A2{
     }
 }
 public class reflection {
-    public static void main(String []args){
+    public static void main(String []args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         A2 new_A=new A2();
         Class cl=new_A.getClass();
         Method []m=cl.getMethods();
         System.out.println(cl.getName());
+        for(Method m1:m){
+            System.out.println(m1.getName());
+        }
+            m[1].invoke(new_A,null);
 
     }
     
