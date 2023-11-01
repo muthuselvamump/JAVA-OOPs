8f7d09d855e3cb098401252f68949ad88d577609
muthuselvamump <muthuselvamump@gmail.com>
2023-11-01 12:11:25 +0530

    Enumeration

---
+++ Enumeration.java
@@ -0,0 +1,12 @@
+enum levels{
+    EASY,MEDIUM,HARD;
+
+}
+public class Enumeration {
+    public static void main(String []args){
+        levels l=levels.EASY;
+
+        System.out.println(l);
+    }
+    
+}
