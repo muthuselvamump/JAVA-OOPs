1adb4d81732a1b9a57a3050ca998641b3fb6799d
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2023-12-19 13:02:39 +0530

    java user input

--- java_user_input.java
+++ java_user_input.java
@@ -4,7 +4,13 @@ public class java_user_input {
         Scanner scan=new Scanner(System.in);
         System.out.println("Enter a name");
         String name=scan.nextLine();
+        System.out.println("Enter a age");
+        int age=scan.nextInt();
+        System.out.println("Enter a salary");
+        double salary=scan.nextDouble();
         System.out.println("your name is: " + name);
+        System.out.println("your age is: " + age);
+        System.out.println("your salary is: " + salary);
 
     }
     
