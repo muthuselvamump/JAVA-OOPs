94939f04873755ccb1bb3ce659e03c9d45bee60b
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2023-12-20 17:08:25 +0530

    Local and and Time class

--- java_date.java
+++ java_date.java
@@ -3,6 +3,10 @@ public class java_date {
     public static void main(String []args){
         LocalDate d=LocalDate.now();
         System.out.println("current date is: " + d);
+        LocalTime t=LocalTime.now();
+        System.out.println("Current Time is: " + t);
+        LocalDateTime dt=LocalDateTime.now();
+        System.out.println("Current Date and Time: " + dt);
     }
     
 }
