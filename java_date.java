04246afb926f551626b3488a54f96745b2555dc0
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2023-12-20 17:04:25 +0530

    Local and and Time class

---
+++ java_date.java
@@ -0,0 +1,8 @@
+import java.time.*;
+public class java_date {
+    public static void main(String []args){
+        LocalDate d=LocalDate.now();
+        System.out.println("current date is: " + d);
+    }
+    
+}
