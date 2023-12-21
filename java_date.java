ff0cdb9d90a589f32319a42828999bad17a23d39
muthuselvamump <muthuselvamump@gmail.com>
2023-12-21 22:24:48 +0530

    java_date_class

--- java_date.java
+++ java_date.java
@@ -1,4 +1,5 @@
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 public class java_date {
     public static void main(String []args){
         LocalDate d=LocalDate.now();
@@ -7,6 +8,9 @@ public class java_date {
         System.out.println("Current Time is: " + t);
         LocalDateTime dt=LocalDateTime.now();
         System.out.println("Current Date and Time: " + dt);
+        DateTimeFormatter date=DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formatdate=dt.format(date);
+        System.out.println("formated date and time: "+formatdate);
     }
     
 }
