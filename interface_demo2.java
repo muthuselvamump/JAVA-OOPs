0b921c17a24203cfcf2e1d59410837174a5f40ff
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2023-12-24 18:21:11 +0530

    interface_comparable_demo

--- interface_demo2.java
+++ interface_demo2.java
@@ -1,9 +1,20 @@
-class employee{
+import java.util.Arrays;
+
+class employee implements Comparable <employee>{
     double salary;
     String name;
     employee(String n,double s){
         salary=s;
         name=n;
+    }
+    public int compareTo(employee e){
+        if(salary==e.salary){
+            return 0;
+        }
+        if(salary<e.salary){
+            return -1;
+        }
+        return 1;
     }
 }
 public class interface_demo2{
@@ -14,6 +25,11 @@ public class interface_demo2{
         err[2]=new employee("mark", 50000);
         err[3]=new employee("elon",800000);
         err[4]=new employee("crist", 100000);
+        for(employee e:err){
+            System.out.println(e.name + " " + e.salary);
+        }
+        System.out.println("----------");
+        Arrays.sort(err);
         for(employee e:err){
             System.out.println(e.name + " " + e.salary);
         }
