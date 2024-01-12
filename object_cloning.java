f04870ba01b782201535e5c74c75d1999b463f53
muthuselvamump <muthuselvamump@gmail.com>
2024-01-12 11:11:22 +0530

    gfg

--- object_cloning.java
+++ object_cloning.java
@@ -9,7 +9,7 @@ class students implements Cloneable{
 students(String n,int rn,int di,String dn){
         name=n;
         rollno=rn;
-        dept.id=di;
+       dept.id=di;
         dept.name=dn;
     }
     protected Object clonObject() throws CloneNotSupportedException{
@@ -20,8 +20,11 @@ students(String n,int rn,int di,String dn){
 public class object_cloning {
     public static void main(String []args) throws CloneNotSupportedException{
         students s1= new students("vijay",1520,2,"physics");
-        students s2=(students)s1.clonObject();
-
+        //students s2=(students)s1.clonObject();
+       // System.out.println(s1.dept.name);
+       s1.dept.id=5;
+       s1.dept.name="hukhy";
+        
 
     }
     
