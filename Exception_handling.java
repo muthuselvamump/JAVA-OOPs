064e09782c4410bb8e1c9cb65823411f085d75c9
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-01-15 21:08:44 +0530

    Exception Handling

--- Exception_handling.java
+++ Exception_handling.java
@@ -3,12 +3,27 @@ public class Exception_handling {
         int a=10;
         int b=0;
         int c=0;
+        int []arr=null;
         try{
             c=a/b;
+            System.out.println(arr[0]);
         }
         catch(Exception d){
             System.out.println("Error");
 
+        }
+        try{
+            System.out.println(arr[1]);
+            c=a/b;
+            
+            
+        }
+       /*  catch(ArithmeticException d){
+            System.out.println("Error try 2");
+
+        }*/
+        catch(NullPointerException n){
+            System.out.println("Error null point");
         }
         
         System.out.println(c);
