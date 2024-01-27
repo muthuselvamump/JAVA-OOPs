c359b8e22047384d92e84446940fefcbe7aaf26e
muthuselvamump <muthuselvamump@gmail.com>
2024-01-27 20:20:28 +0530

    Exception_handling2

---
+++ Exception_handling2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Exception_handling2{
+    public static void main(String []args){
+        Scanner scan=new Scanner(System.in);
+        int i=scan.nextInt();
+        //int j=scan.nextDouble();
+        //String s="hrllo";
+        //double d=(double)s;
+        System.out.println(i);
+        //important----->>>>>scan.close();//proper closed resources
+        //if we use into try it getting automatically close
+        int k;
+      try(Scanner scanner=new Scanner(System.in)){ //its called try with resources
+       int j=scanner.nextInt(); 
+         k=scanner.nextInt();  
+        }
+        System.out.println("its k"+ k );//if we create varaible into try we can use into try only
+    }
+    
+}
