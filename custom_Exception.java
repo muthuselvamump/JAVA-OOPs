7c73fdbff353522f0e7245c17638e11ec8f1ae9f
muthuselvamump <muthuselvamump@gmail.com>
2024-01-28 21:54:04 +0530

    custom_Exception_handling

--- custom_Exception.java
+++ custom_Exception.java
@@ -1,10 +1,14 @@
 
 public class custom_Exception extends Exception {
+    int amo;
+    custom_Exception(int a){
+        amo=a;
+    }
     public static void main(String []args){
         int bal=67,amo=75;
         try{
         if(bal<amo){
-            throw new custom_Exception();
+            throw new custom_Exception(amo);
         }
         }
         catch(custom_Exception c){
