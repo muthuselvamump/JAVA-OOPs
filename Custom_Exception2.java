af7e650134250b7c0c2dcc320add11e97e4d12a8
muthuselvamump <muthuselvamump@gmail.com>
2024-01-30 11:45:05 +0530

    custom_Exception_handling

--- Custom_Exception2.java
+++ Custom_Exception2.java
@@ -3,10 +3,20 @@ public class Custom_Exception2 extends Exception{
     Custom_Exception2(int a){
         balance=a;
     }
+    Custom_Exception2(){
+
+    }
     public int withdraw(int a){
         if(a>balance){
             return a-balance;
         }
+        else{
+            Custom_Exception2.withdraw1();
+            return -1;
+        }
+    }
+    public static void withdraw1(){
+
     }
     public static void main(String []args){
         Custom_Exception2 c1=new Custom_Exception2(300);
