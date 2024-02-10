af57f2c7440ce9369b25ba392fa4c24a162ae2eb
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-02-10 20:10:57 +0530

    Add files via upload

--- multithreading.java
+++ multithreading.java
@@ -1,10 +1,20 @@
 class name{
     void printname(){
-
+        String []srr={"vijay","ajith","surya","vikram","anirudh","muthuselvam","ganesh","aakhash","vasanth","praveen"};
+       // srr[1]="raj";
+        for(int i=0;i<10;i++){
+            System.out.println(srr[i]);
+            try{
+            Thread.sleep(6000);
+            }catch(Exception e){}
+        }
     }
 }
 class numb{
     void printnumb(){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
 
     }
 }
@@ -12,6 +22,8 @@ public class multithreading{
     public static void main(String args[]){
         name n=new name();
         numb num=new numb();
+        n.printname();
+        num.printnumb();
 
     }
 }
