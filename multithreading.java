24dda077fe1a164147bf8d353e9bc74a755768b0
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-02-10 19:55:56 +0530

    Add files via upload

---
+++ multithreading.java
@@ -0,0 +1,17 @@
+class name{
+    void printname(){
+
+    }
+}
+class numb{
+    void printnumb(){
+
+    }
+}
+public class multithreading{
+    public static void main(String args[]){
+        name n=new name();
+        numb num=new numb();
+
+    }
+}
