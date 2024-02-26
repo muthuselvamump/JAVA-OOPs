f2094f520768f1268126cfc23a1d9cc895282c0e
muthuselvamump <muthuselvamump@gmail.com>
2024-02-26 21:40:05 +0530

    multi_threading_using_interface

--- multi_threading_using_interface.java
+++ multi_threading_using_interface.java
@@ -1,7 +1,8 @@
-class threading1{
-    public void printnum(){
+class threading1 implements Runnable{
+    public void run(){
         for(int i=1;i<11;i++){
             System.out.println(i);
+           try{ Thread.sleep(2000);}catch(Exception E){}
         }
     }
 
@@ -11,7 +12,6 @@ class threading2{
         for(int i=1;i<11;i++){
             System.out.println("java");
         }
-
     }
 
 }
@@ -19,7 +19,12 @@ public class multi_threading_using_interface {
     public static void main(String []args){
         threading1 t1= new threading1();
         threading2 t2=new threading2();
-        t1.printnum();
+        Runnable r1=new threading1();
+        Thread t3=new Thread(r1);
+            t3.start();
+            t2.printname();
+    //    t1.run();
+    
     }
     
     
