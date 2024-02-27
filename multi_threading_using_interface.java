df70543f68ced679b78643286f7dccd5d749f6e0
muthuselvamump <muthuselvamump@gmail.com>
2024-02-27 21:52:25 +0530

    multi_threading_using_interface

--- multi_threading_using_interface.java
+++ multi_threading_using_interface.java
@@ -2,7 +2,7 @@ class threading1 implements Runnable{
     public void run(){
         for(int i=1;i<11;i++){
             System.out.println(i);
-           try{ Thread.sleep(2000);}catch(Exception E){}
+           try{ Thread.sleep(4000);}catch(Exception E){}
         }
     }
 
@@ -11,6 +11,10 @@ class threading2{
     public void printname(){
         for(int i=1;i<11;i++){
             System.out.println("java");
+            try{
+                Thread.sleep(2000);
+            }
+            catch(Exception e){}
         }
     }
 
@@ -23,6 +27,7 @@ public class multi_threading_using_interface {
         Thread t3=new Thread(r1);
             t3.start();
             t2.printname();
+            System.out.println("End of program");
     //    t1.run();
     
     }
