4a4e84e455864f78c4bdc850b353cba860d6f5e4
muthuselvamump <muthuselvamump@gmail.com>
2024-03-06 18:11:42 +0530

    multithreading_demo2

--- multithreading_demo2.java
+++ multithreading_demo2.java
@@ -2,21 +2,26 @@ public class multithreading_demo2 extends Thread{
     public void run(){
         for(int i=0;i<6;i++){
             System.out.println("Low Priority");
+           // try{Thread.sleep(6000);}catch(Exception e){};
         }
     }
     public static void main(String []args){
         multithreading_demo2 m1= new multithreading_demo2();
-        m1.start();
+        
+    
         multithreading_demo2 m2=new multithreading_demo2(){
           public void run(){
             for(int i=0;i<6;i++){                
-                try{Thread.sleep(6000);}catch(Exception e){};
-                System.out.println("High priority");            
+                System.out.println("High priority");
+                try{Thread.sleep(6000);}catch(Exception e){};            
             } 
         }  
         };
-        
+        m2.setPriority(MAX_PRIORITY);
+        m1.setPriority(MIN_PRIORITY);    
+        m1.start();
         m2.start();
+        
     }
     
 }
