c59de30b4d31270f93027537778992f200a82ee8
muthuselvamump <muthuselvamump@gmail.com>
2024-03-07 12:10:11 +0530

    multithreading_synchronize

---
+++ multithreading_synchronize.java
@@ -0,0 +1,14 @@
+class synchro{
+    public void table(int a){
+
+        for(int i=1;i<=a;i++){
+            System.out.println(a*i);        }
+
+    }
+}
+public class multithreading_synchronize {
+    public static void main(String []args){
+        T   H
+    }
+    
+}
