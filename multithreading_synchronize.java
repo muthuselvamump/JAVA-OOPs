9687df7c16a27b324372146ed96a84c9ab2f6223
muthuselvamump <muthuselvamump@gmail.com>
2024-03-08 11:18:43 +0530

    multithreading_synchronize

--- multithreading_synchronize.java
+++ multithreading_synchronize.java
@@ -1,28 +1,20 @@
 class synchro{
-    public void table(int a){
+    synchronized public void table(int a){
 
         for(int i=1;i<=a;i++){
-            System.out.println(a*i);        }
+            System.out.println(a*i); 
+            try{Thread.sleep(1000);}catch(Exception e){}
+        }
+
 
     }
 }
 public class multithreading_synchronize {
     public static void main(String []args){
-        synchro s1=new synchro();
-        synchro s2=new synchro();
+        synchro s1=new synchro();//if two thread access method with same object 
+        synchro s2=new synchro();//if we want at a time one thread can be access method we can use synchronized method
         Thread t1=new Thread(){
-            public void run(){
-                s1.table(5);
-            }
-        };
-        Thread t2=new Thread(){
-            public void run(){
-                s2.table(6);
-            }
-        };
-        t1.start();
-        t2.start();
-
+     
     }
     
 }
