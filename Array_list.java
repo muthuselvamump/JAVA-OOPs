92bfc5a21c60de03d538fe13bc5f5a8d98837892
muthuselvamump <muthuselvamump@gmail.com>
2024-03-09 11:42:43 +0530

    Array_list

---
+++ Array_list.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.ArrayList;
+public class Array_list {
+    public static void main(String args[]){
+        int arr[]=new int[5];//array is fixed size
+        ArrayList<Integer> all=new ArrayList<Integer>();//arraylist is flexible
+
+    }    
+}
