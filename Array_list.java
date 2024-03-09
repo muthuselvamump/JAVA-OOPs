79d4b03b9107a423fa5eb2871c8bb0a182c017cd
muthuselvamump <muthuselvamump@gmail.com>
2024-03-09 11:49:19 +0530

    Array_list

--- Array_list.java
+++ Array_list.java
@@ -4,6 +4,10 @@ public class Array_list {
     public static void main(String args[]){
         int arr[]=new int[5];//array is fixed size
         ArrayList<Integer> all=new ArrayList<Integer>();//arraylist is flexible
+        all.add(59);
+        System.out.println(all.indexOf(59));
+        System.out.println(all.get(0));
+
 
     }    
 }
