6b3eaeee1129eabeccd5de61520905e80ec11177
muthuselvamump <muthuselvamump@gmail.com>
2024-03-19 23:47:24 +0530

    collection

--- linkedlistdemo.java
+++ linkedlistdemo.java
@@ -2,5 +2,18 @@ import java.util.LinkedList;
 public class linkedlistdemo{
 public static void main(String[] args) {
     LinkedList<Integer> l1=new LinkedList<Integer>();
+    l1.add(5);
+    l1.add(8);
+    l1.add(3);
+    l1.add(9);
+    l1.add(56);
+    System.out.println(l1);
+    System.out.println(l1.get(2));
+    l1.set(2,46);
+
+    System.out.println("loop");
+    for(int i:l1){
+        System.out.println("linke list element "+i);
+    }
 }
 }
