cbd8632b76ebb94df9772bef316b0bf610bf8f54
muthuselvamump <muthuselvamump@gmail.com>
2024-03-20 21:41:17 +0530

    collection

---
+++ linkedlistdemo2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class linkedlistdemo2 {
+    public static void main(String []args){
+        LinkedList<Integer> l1=new LinkedList<Integer>();
+        l1.add(9);l1.add(1);
+        l1.add(2);
+        l1.add(4);
+        l1.add(8);
+        l1.add(6);
+        l1.add(4);
+        System.out.println(l1);
+        l1.addFirst(2);
+        System.out.println(l1);
+        l1.addLast(5);
+        System.out.println(l1);
+        System.out.println(l1.getLast());
+        System.out.println(l1.getFirst());
+        System.out.println(l1);
+        l1.removeFirst();
+        System.out.println(l1);
+        l1.removeLast();
+        System.out.println(l1);
+    }
+    
+}
