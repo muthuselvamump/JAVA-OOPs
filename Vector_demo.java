9c03a72bd11abe3787abee7800ef656285410455
muthuselvamump <muthuselvamump@gmail.com>
2024-03-21 21:37:02 +0530

    collection

---
+++ Vector_demo.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class Vector_demo {
+    public static void main(String args[]){
+        Vector<Integer> v1=new Vector<Integer>();
+        /*v1.add(55);
+        v1.add(7);
+        v1.add(55);
+        v1.add(7);
+        v1.add(55);
+        v1.add(7);
+        v1.add(55);
+        v1.add(7);
+        v1.add(55);
+        v1.add(7);
+        v1.add(55);
+        v1.add(7);*/
+        System.out.println(v1);
+        System.out.println(v1.size());
+    }
+    
+}
