7989386ac157094c866463de9e0bac98996ef33b
muthuselvamump <muthuselvamump@gmail.com>
2024-03-25 22:54:39 +0530

    COLLECTION_DEMO

---
+++ queue_demo.java
@@ -0,0 +1,12 @@
+import java.util.PriorityQueue;
+
+public class queue_demo{
+    public static void main(String []args){
+    PriorityQueue<Integer> q=new PriorityQueue<Integer>();
+    q.add(2);
+    q.add(6);
+    q.add(4);
+    q.add(7);
+    System.out.println(q);
+    }
+}
