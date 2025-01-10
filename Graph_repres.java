b2bc36c3d56462e331d43fe0475c20d814fea562
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2025-01-10 23:58:08 +0530

    Graph Representation

--- Graph_repres.java
+++ Graph_repres.java
@@ -9,6 +9,6 @@ public class Graph_repres {
                      {0,1,0,1,0},
                      {0,0,1,0,1},
                      {1,1,0,1,0}};
-        
+        System.out.println(arr[4][0]);
     }
 }
